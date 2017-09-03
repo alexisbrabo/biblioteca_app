0b18fc1d30c666fac96a21a9270f5e9126f91824
alexi <alexi@192.168.1.9>
2017-09-03 09:25:05 -0300

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/java/br/com/alexis/outros/Uteis.java
    	src/java/br/com/alexis/views/indexView.java
    	web/index.xhtml

---
+++ src/java/br/com/alexis/beans/LivroBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.alexis.beans;
+
+import br.com.alexis.models.Livro;
+import br.com.alexis.padrao.Bean;
+import java.util.List;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author alexi
+ */
+@Stateless
+public class LivroBean extends Bean {
+
+    public List<Livro> findAll() {
+        return em.createQuery("SELECT o FROM Livro o WHERE o.id > 0").getResultList();
+    }
+
+}
